8c67e1d86b7b8ecc17cc8f2ad5afbfb474f8b954
Tara L Andrews <tla@mit.edu>
2022-12-02 01:25:58 +0100

    Figure out real ways to do things; remove functions

--- advent1/src/advent1/core.clj
+++ advent1/src/advent1/core.clj
@@ -2,24 +2,8 @@
   (:gen-class))
 (require '[clojure.string :as str])
 
-(defn sum
-  "Recursively add up items of a list"
-  ([items] (sum items 0))
-  ([items total]
-    (if (empty? items)
-      total
-      (recur (rest items) (+ (first items) total)))))
-
-(defn collect
-  "Flattens two lists of items into one list. There must be a real way to do this"
-  ([items] (collect items []))
-  ([items collection] 
-    (if (empty? items)
-      collection
-      (recur (rest items) (conj collection (first items))))))
-
 (defn group
-  "Split a sequence by a delimiter, eating the delimiter"
+  "Split a sequence into multiple sequences by a delimiter, eating the delimiter"
   ([delim items] (group delim items '()))
   ([delim items collected]
     (if (empty? items)
@@ -28,7 +12,7 @@
         ; Chomp off this delimiter and continue
         (recur delim (rest items) (conj collected '()))
         ; Add this item to the end of the first list in collected
-        (recur delim (rest items) (conj (rest collected) (collect [(Integer. (first items))] (first collected))))))))
+        (recur delim (rest items) (conj (rest collected) (conj (first collected) (Integer. (first items)))))))))
 
 (defn -main
   "Calorie counting"
@@ -36,5 +20,4 @@
   ; Slurp in the input file
   (println (str "Running on file " (first *command-line-args*)))
   (def input (str/split (slurp (first *command-line-args*)) #"\n"))
-  (println (str "Most loaded elf has " (apply max (map sum (group "" input))) " calories"))
-  )
+  (println (str "Most loaded elf has " (apply max (map #(reduce + %) (group "" input))) " calories")))
