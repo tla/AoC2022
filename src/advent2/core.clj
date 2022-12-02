d0f8ebba40e4dc5e9b01e5e16a2718f3529fea82
Tara L Andrews <tla@mit.edu>
2022-12-02 23:44:01 +0100

    oh hey there was a part 2

--- src/advent2/core.clj
+++ src/advent2/core.clj
@@ -5,17 +5,30 @@
 ;; I wanted to do this one without making a boring map like
 ;; {"A Z" -> 3, "A X" -> 7} and so on. So I reduced each letter to 
 ;; the value 0, 1, 2 for rock, paper, scissors respectively and then
-;; used some mod-3 arithmetic to determine the winner. 
+;; used some mod-3 arithmetic to determine the winner.
 
 (defn win-points
   "Return the number of points for a given play. 
-   6 for victory, 3 for tie, 0 for loss"
+   6 for victory, 3 for tie, 0 for loss plus (cardval + 1)"
   [their-play my-play]
-  (let [result (mod (- my-play their-play) 3)]
-    (case result
-      0 3
-      1 6
-      2 0)))
+  (let [result (mod (+ 1 (- my-play their-play)) 3)]
+    (+ my-play 1 (* result 3))))
+
+;; Alternatively we have to figure out how many points we got
+;; based on whether we won, lost, or tied as indicated by the 
+;; X Y Z. This means we have to reverse some of the math in
+;; win-points above.
+
+(defn gaming-points
+  "Figure out how many points we get if we win, draw, or lose on spec"
+  [their-play my-play]
+  (+ (* 3 my-play) ;; Get the win/draw/loss points, easy
+     (case my-play ;; Figure out what card we must have played.
+                   ;; Not going to try to avoid `case`; it's late.
+       0 (+ 1 (mod (+ their-play 2) 3))
+       1 (+ 1 their-play)
+       2 (+ 1 (mod (+ their-play 1) 3)))
+     ))
 
 ;; The score is, for some reason, a combination of the win points
 ;; and the value of the play itself. Well that's easy, just add 1
@@ -23,11 +36,12 @@
 
 (defn score
   "Return the score for a given play such as 'A Y'"
-  [play]
+  [play point-calculator]
   ;; Turn the letters into integers
   (let [their-play (- (int (get play 0)) 65)
         my-play (- (int (get play 2)) 88)
-        score (+ my-play 1 (win-points their-play my-play))] 
+        score (point-calculator their-play my-play)] 
+    (println "Play was" play ", score was" score)
     score))
 
 ;; Now just sum it all up for the whole input.
@@ -37,5 +51,6 @@
   [& args] 
   (println "Running on file" (first args))
   (let [input (str/split (slurp (first args)) #"\n")] 
-    (println "We would have" (apply + (map score input)) "points in this game."))
+    (println "We would have" (apply + (map #(score % win-points) input)) "points at first.")
+    (println "We would have" (apply + (map #(score % gaming-points) input)) "points in the end."))
 )
